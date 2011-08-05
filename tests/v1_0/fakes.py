4a4e9a60e81702041dcc225325dea0c9cba7487a
Matt Dietz <matt.dietz@rackspace.com>
2011-08-05 13:23:34 -0500

    Removed the bodies again

--- tests/v1_0/fakes.py
+++ tests/v1_0/fakes.py
@@ -26,7 +26,7 @@ class FakeHTTPClient(base_client.HTTPClient):
         # Check that certain things are called correctly
         if method in ['GET', 'DELETE']:
             assert 'body' not in kwargs
-        elif method in ['PUT', 'POST']:
+        elif method == 'PUT':
             assert 'body' in kwargs
 
         # Call the method
@@ -159,6 +159,9 @@ class FakeHTTPClient(base_client.HTTPClient):
             for pfile in body['server']['personality']:
                 fakes.assert_has_keys(pfile, required=['path', 'contents'])
         return (202, self.get_servers_1234()[1])
+    
+    def post_servers_1234_migrate(self, *args, **kwargs):
+        return (202, None)
 
     def get_servers_1234(self, **kw):
         r = {'server': self.get_servers_detail()[1]['servers'][0]}
