149e00bcf5eeb16e4e5cc13840cdb6d0fe2897e3
Josh Kearney <josh@jk0.org>
2011-11-16 15:21:25 -0600

    Adds bash completion support and cleans up setup.py.
    Also removes some unnecessary files and updates the AUTHORS file.
    
    Change-Id: Iefa502ce08cbf34038d2e04d0460ae43468a4255

--- setup.py
+++ setup.py
@@ -1,40 +1,54 @@
+# Copyright 2011 OpenStack, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
+import setuptools
 import sys
-from setuptools import setup, find_packages
 
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-requirements = ['httplib2', 'argparse', 'prettytable']
+requirements = ["httplib2", "argparse", "prettytable"]
 if sys.version_info < (2, 6):
-    requirements.append('simplejson')
+    requirements.append("simplejson")
 
-setup(
-    name = "python-novaclient",
-    version = "2.6.7",
-    description = "Client library for OpenStack Nova API",
-    long_description = read('README.rst'),
-    url = 'https://github.com/rackspace/python-novaclient',
-    license = 'Apache',
-    author = 'Rackspace, based on work by Jacob Kaplan-Moss',
-    author_email = 'github@racklabs.com',
-    packages = find_packages(exclude=['tests', 'tests.*']),
-    classifiers = [
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
+
+def read_file(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
+
+setuptools.setup(
+    name="python-novaclient",
+    version="2.6.7",
+    author="Rackspace, based on work by Jacob Kaplan-Moss",
+    author_email="github@racklabs.com",
+    description="Client library for OpenStack Nova API.",
+    long_description=read_file("README.rst"),
+    license="Apache License, Version 2.0",
+    url="https://github.com/openstack/python-novaclient",
+    packages=["novaclient"],
+    install_requires=requirements,
+    tests_require=["nose", "mock"],
+    test_suite="nose.collector",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python"
     ],
-    install_requires = requirements,
-
-    tests_require = ["nose", "mock"],
-    test_suite = "nose.collector",
-
-    entry_points = {
-        'console_scripts': ['nova = novaclient.shell:main']
+    entry_points={
+        "console_scripts": ["nova = novaclient.shell:main"]
     }
 )
