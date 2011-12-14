1acbbb0c2d1fc152102635f0b7f66910a945b479
Monty Taylor <mordred@inaugust.com>
2011-12-14 10:14:08 -0500

    Align run_tests.sh with nova.
    We need for run_tests.sh to accept the same inputs and outputs from a CI
    perspective. In this case, the easiest way was just to port in run_test.py.
    Additionally, we need with_venv.sh to exist.
    
    Change-Id: I25e659ed796c8a70239aab610f5b014b35443b67

--- run_tests.sh
+++ run_tests.sh
@@ -1,116 +1,153 @@
 #!/bin/bash
-#
-# Copyright 2011, Piston Cloud Computing, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
+set -eu
 
 function usage {
-    echo "Usage: $0 [OPTION] [nosearg1[=val]] [nosearg2[=val]]..."
-    echo 
-    echo "Run python-novaclient test suite"
-    echo
-    echo "  -f, --force             Delete the virtualenv before running tests."
-    echo "  -h, --help              Print this usage message"
-    echo "  -N, --no-virtual-env    Don't use a virtualenv" 
-    echo "  -p, --pep8              Run pep8 in addition"
+  echo "Usage: $0 [OPTION]..."
+  echo "Run python-novaclient test suite"
+  echo ""
+  echo "  -V, --virtual-env        Always use virtualenv.  Install automatically if not present"
+  echo "  -N, --no-virtual-env     Don't use virtualenv.  Run tests in local environment"
+  echo "  -s, --no-site-packages   Isolate the virtualenv from the global Python environment"
+  echo "  -x, --stop               Stop running tests after the first error or failure."
+  echo "  -f, --force              Force a clean re-build of the virtual environment. Useful when dependencies have been added."
+  echo "  -p, --pep8               Just run pep8"
+  echo "  -P, --no-pep8            Don't run pep8"
+  echo "  -c, --coverage           Generate coverage report"
+  echo "  -h, --help               Print this usage message"
+  echo "  --hide-elapsed           Don't print the elapsed time for each test along with slow test list"
+  echo ""
+  echo "Note: with no options specified, the script will try to run the tests in a virtual environment,"
+  echo "      If no virtualenv is found, the script will ask if you would like to create one.  If you "
+  echo "      prefer to run tests NOT in a virtual environment, simply pass the -N option."
+  exit
 }
 
-
-function die {
-    echo $@
-    exit 1
+function process_option {
+  case "$1" in
+    -h|--help) usage;;
+    -V|--virtual-env) always_venv=1; never_venv=0;;
+    -N|--no-virtual-env) always_venv=0; never_venv=1;;
+    -s|--no-site-packages) no_site_packages=1;;
+    -f|--force) force=1;;
+    -p|--pep8) just_pep8=1;;
+    -P|--no-pep8) no_pep8=1;;
+    -c|--coverage) coverage=1;;
+    -*) noseopts="$noseopts $1";;
+    *) noseargs="$noseargs $1"
+  esac
 }
 
-
-function process_args {
-    case "$1" in
-        -h|--help) usage && exit ;;
-        -p|--pep8) let just_pep8=1; let use_venv=0 ;;
-        -N|--no-virtual-env) let use_venv=0;;
-        -f|--force) let force=1;;
-        *) noseargs="$noseargs $1"
-    esac
-}
-
-
-function run-command {
-    res=$($@)
-
-    if [ $? -ne 0 ]; then
-        die "Command failed:", $res
-    fi
-}
-
-
-function install-dependency {
-    echo -n "installing $@..."
-    run-command "pip install -E $venv $@"
-    echo done
-}
-
-
-function build-venv-if-necessary {
-    if [ $force -eq 1 ]; then
-        echo -n "Removing virtualenv..."
-        rm -rf $venv
-        echo done
-    fi
-
-    if [ -d $venv ]; then 
-        echo -n  # nothing to be done
-    else
-        if [ -z $(which virtualenv) ]; then 
-            echo "Installing virtualenv"
-            run-command "easy_install virtualenv"
-        fi
-
-        echo -n "creating virtualenv..."
-        run-command "virtualenv -q --no-site-packages ${venv}"
-        echo done
-
-        for dep in $dependencies; do 
-            install-dependency $dep
-        done
-    fi
-}
-
-
-function wrapper {
-    if [ $use_venv -eq 1 ]; then
-        build-venv-if-necessary
-        source "$(dirname $0)/${venv}/bin/activate" && $@
-    else
-        $@
-    fi
-}
-
-
-dependencies="httplib2 argparse prettytable simplejson nose mock coverage"
+venv=.venv
+with_venv=tools/with_venv.sh
+always_venv=0
+never_venv=0
 force=0
-venv=.novaclient-venv
-use_venv=1
-verbose=0
+no_site_packages=0
+installvenvopts=
 noseargs=
+noseopts=
+wrapper=""
 just_pep8=0
+no_pep8=0
+coverage=0
 
-for arg in "$@"; do 
-    process_args $arg
+for arg in "$@"; do
+  process_option $arg
 done
 
-NOSETESTS="nosetests ${noseargs}"
+# If enabled, tell nose to collect coverage data 
+if [ $coverage -eq 1 ]; then
+    noseopts="$noseopts --with-coverage --cover-package=novaclient"
+fi
 
-if [ $just_pep8 -ne 0 ]; then
-    wrapper "pep8 -r --show-pep8 novaclient tests"
-else
-    wrapper $NOSETESTS
+if [ $no_site_packages -eq 1 ]; then
+  installvenvopts="--no-site-packages"
+fi
+
+function run_tests {
+  # Just run the test suites in current environment
+  ${wrapper} $NOSETESTS
+  # If we get some short import error right away, print the error log directly
+  RESULT=$?
+  return $RESULT
+}
+
+function run_pep8 {
+  echo "Running pep8 ..."
+  srcfiles="novaclient tests"
+  # Just run PEP8 in current environment
+  #
+  # NOTE(sirp): W602 (deprecated 3-arg raise) is being ignored for the
+  # following reasons:
+  #
+  #  1. It's needed to preserve traceback information when re-raising
+  #     exceptions; this is needed b/c Eventlet will clear exceptions when
+  #     switching contexts.
+  #
+  #  2. There doesn't appear to be an alternative, "pep8-tool" compatible way of doing this
+  #     in Python 2 (in Python 3 `with_traceback` could be used).
+  #
+  #  3. Can find no corroborating evidence that this is deprecated in Python 2
+  #     other than what the PEP8 tool claims. It is deprecated in Python 3, so,
+  #     perhaps the mistake was thinking that the deprecation applied to Python 2
+  #     as well.
+  ${wrapper} pep8 --repeat --show-pep8 --show-source \
+    --ignore=E202,W602 \
+    ${srcfiles}
+}
+
+NOSETESTS="nosetests $noseopts $noseargs"
+
+if [ $never_venv -eq 0 ]
+then
+  # Remove the virtual environment if --force used
+  if [ $force -eq 1 ]; then
+    echo "Cleaning virtualenv..."
+    rm -rf ${venv}
+  fi
+  if [ -e ${venv} ]; then
+    wrapper="${with_venv}"
+  else
+    if [ $always_venv -eq 1 ]; then
+      # Automatically install the virtualenv
+      python tools/install_venv.py $installvenvopts
+      wrapper="${with_venv}"
+    else
+      echo -e "No virtual environment found...create one? (Y/n) \c"
+      read use_ve
+      if [ "x$use_ve" = "xY" -o "x$use_ve" = "x" -o "x$use_ve" = "xy" ]; then
+        # Install the virtualenv and run the test suite in it
+        python tools/install_venv.py $installvenvopts
+        wrapper=${with_venv}
+      fi
+    fi
+  fi
+fi
+
+# Delete old coverage data from previous runs
+if [ $coverage -eq 1 ]; then
+    ${wrapper} coverage erase
+fi
+
+if [ $just_pep8 -eq 1 ]; then
+    run_pep8
+    exit
+fi
+
+run_tests
+
+# NOTE(sirp): we only want to run pep8 when we're running the full-test suite,
+# not when we're running tests individually. To handle this, we need to
+# distinguish between options (noseopts), which begin with a '-', and
+# arguments (noseargs).
+if [ -z "$noseargs" ]; then
+  if [ $no_pep8 -eq 0 ]; then
+    run_pep8
+  fi
+fi
+
+if [ $coverage -eq 1 ]; then
+    echo "Generating coverage report in covhtml/"
+    ${wrapper} coverage html -d covhtml -i
 fi
