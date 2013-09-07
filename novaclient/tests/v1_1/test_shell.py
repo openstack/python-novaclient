28f97734203a2df33805291673104742d45d34f3
Chuck Short <chuck.short@canonical.com>
2013-09-07 12:27:46 -0400

    Python3: Use six.StringIO for io.Bytes()
    The newer version of six (1.4.1) provides six.StringIO which
    is a fake file object for textual data. It's an alias for StringIO.StringIO
    in python2 and io.StringIO in Python3.
    
    Use the fake object where approiate.
    
    Change-Id: I364001933b4f2305ac27b293a9a4a3fec36c8a49
    Signed-off-by: Chuck Short <chuck.short@canonical.com>

--- novaclient/tests/v1_1/test_shell.py
+++ novaclient/tests/v1_1/test_shell.py
@@ -17,13 +17,13 @@
 #    under the License.
 
 import datetime
-import io
 import os
 import mock
 import sys
 import tempfile
 
 import fixtures
+import six
 
 import novaclient.client
 from novaclient import exceptions
@@ -71,7 +71,7 @@ class ShellTest(utils.TestCase):
             lambda *_: fakes.FakeClient))
         self.addCleanup(timeutils.clear_time_override)
 
-    @mock.patch('sys.stdout', io.BytesIO())
+    @mock.patch('sys.stdout', six.StringIO())
     def run_command(self, cmd):
         if isinstance(cmd, list):
             self.shell.main(cmd)
