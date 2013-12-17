3b5b7b9da3a36269a1999dea6cda04095bf390cd
Chris Yeoh <cyeoh@au1.ibm.com>
2013-12-17 16:21:41 +1030

    Adds simple tenant usage support for the Nova V3 API
    Adds support and tests for the os-simple-tenant-usage
    extension for the Nova V3 API. There are no
    differences between the V2 and V3 API and this change
    adds the client code to enable the usage command. Note
    that with nomenclature change from tenant to project
    occuring in Nova there will be a difference between
    the V2 and V3 API in the future.
    
    Differences between the V2 and V3 API are described here:
    https://wiki.openstack.org/wiki/NovaAPIv2tov3
    
    Partially implements blueprint v3-api
    
    Change-Id: Iddf6211c9c87cbb191d9a5d5a65710b343c30e67

--- novaclient/tests/v1_1/test_usage.py
+++ novaclient/tests/v1_1/test_usage.py
@@ -18,16 +18,23 @@ from novaclient.tests.v1_1 import fakes
 from novaclient.v1_1 import usage
 
 
-cs = fakes.FakeClient()
-
-
 class UsageTest(utils.TestCase):
+    def setUp(self):
+        super(UsageTest, self).setUp()
+        self.cs = self._get_fake_client()
+        self.usage_type = self._get_usage_type()
+
+    def _get_fake_client(self):
+        return fakes.FakeClient()
+
+    def _get_usage_type(self):
+        return usage.Usage
 
     def test_usage_list(self, detailed=False):
         now = datetime.datetime.now()
-        usages = cs.usage.list(now, now, detailed)
+        usages = self.cs.usage.list(now, now, detailed)
 
-        cs.assert_called('GET',
+        self.cs.assert_called('GET',
                          "/os-simple-tenant-usage?" +
                          ("start=%s&" % now.isoformat()) +
                          ("end=%s&" % now.isoformat()) +
@@ -39,9 +46,9 @@ class UsageTest(utils.TestCase):
 
     def test_usage_get(self):
         now = datetime.datetime.now()
-        u = cs.usage.get("tenantfoo", now, now)
+        u = self.cs.usage.get("tenantfoo", now, now)
 
-        cs.assert_called('GET',
+        self.cs.assert_called('GET',
                          "/os-simple-tenant-usage/tenantfoo?" +
                          ("start=%s&" % now.isoformat()) +
                          ("end=%s" % now.isoformat()))
