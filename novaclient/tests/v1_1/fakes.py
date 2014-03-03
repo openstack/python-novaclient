7c11b06bd4e896d891ed3f50c6480bd76a1023bf
Masayuki Igawa <igawa@mxs.nes.nec.co.jp>
2014-03-03 14:30:56 +0900

    Remove quota-class subcommand
    quota-classes API was already removed.
      Change-Id: I1110022d6f628d03aaf363da707f2d2ef1600437
    This patch removed quota-class subcommand from nova client.
    
    Change-Id: I18bf7c255fabdb52c8ce8159f68c3e5c70e54993

--- novaclient/tests/v1_1/fakes.py
+++ novaclient/tests/v1_1/fakes.py
@@ -1162,56 +1162,6 @@ class FakeHTTPClient(base_client.HTTPClient):
         return (202, {}, {})
 
     #
-    # Quota Classes
-    #
-
-    def get_os_quota_class_sets_test(self, **kw):
-        return (200, {}, {'quota_class_set': {
-                      'id': 'test',
-                      'metadata_items': 1,
-                      'injected_file_content_bytes': 1,
-                      'injected_file_path_bytes': 1,
-                      'ram': 1,
-                      'floating_ips': 1,
-                      'instances': 1,
-                      'injected_files': 1,
-                      'cores': 1,
-                      'key_pairs': 1,
-                      'security_groups': 1,
-                      'security_group_rules': 1}})
-
-    def put_os_quota_class_sets_test(self, body, **kw):
-        assert list(body) == ['quota_class_set']
-        return (200, {}, {'quota_class_set': {
-                      'metadata_items': 1,
-                      'injected_file_content_bytes': 1,
-                      'injected_file_path_bytes': 1,
-                      'ram': 1,
-                      'floating_ips': 1,
-                      'instances': 1,
-                      'injected_files': 1,
-                      'cores': 1,
-                      'key_pairs': 1,
-                      'security_groups': 1,
-                      'security_group_rules': 1}})
-
-    def put_os_quota_class_sets_97f4c221bff44578b0300df4ef119353(self,
-                                                            body, **kw):
-        assert list(body) == ['quota_class_set']
-        return (200, {}, {'quota_class_set': {
-                      'metadata_items': 1,
-                      'injected_file_content_bytes': 1,
-                      'injected_file_path_bytes': 1,
-                      'ram': 1,
-                      'floating_ips': 1,
-                      'instances': 1,
-                      'injected_files': 1,
-                      'cores': 1,
-                      'key_pairs': 1,
-                      'security_groups': 1,
-                      'security_group_rules': 1}})
-
-    #
     # Security Groups
     #
     def get_os_security_groups(self, **kw):
