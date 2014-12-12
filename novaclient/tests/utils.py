ab2f1a742da217ff2de469267c4c4279ebddd4de
Balazs Gibizer <balazs.gibizer@ericsson.com>
2014-12-12 15:09:12 +0100

    Reject non existent mock assert calls
    assert_called and assert_not_called are not asserting the state
    of the mock object but considered as mocked calls so mock
    will never raise exception but always executed successfully
    
    This change patches the Mock class during unit test
    to raise an exception if a function called on a mock object
    that's name starts with 'assert' and does not one
    of the supported Mock assert calls.
    
    This change also fix the unit test to call only the supported
    assert function on mock object.
    
    Change-Id: I036587f355e42e362ac2b70fb6755cca81d30b75

--- novaclient/tests/utils.py
+++ novaclient/tests/utils.py
@@ -14,6 +14,7 @@
 import os
 
 import fixtures
+import mock
 from oslo.serialization import jsonutils
 import requests
 from requests_mock.contrib import fixture as requests_mock_fixture
@@ -24,6 +25,29 @@ import testtools
 AUTH_URL = "http://localhost:5002/auth_url"
 AUTH_URL_V1 = "http://localhost:5002/auth_url/v1.0"
 AUTH_URL_V2 = "http://localhost:5002/auth_url/v2.0"
+
+
+def _patch_mock_to_raise_for_invalid_assert_calls():
+    def raise_for_invalid_assert_calls(wrapped):
+        def wrapper(_self, name):
+            valid_asserts = [
+                'assert_called_with',
+                'assert_called_once_with',
+                'assert_has_calls',
+                'assert_any_calls']
+
+            if name.startswith('assert') and name not in valid_asserts:
+                raise AttributeError('%s is not a valid mock assert method'
+                                     % name)
+
+            return wrapped(_self, name)
+        return wrapper
+    mock.Mock.__getattr__ = raise_for_invalid_assert_calls(
+        mock.Mock.__getattr__)
+
+# NOTE(gibi): needs to be called only once at import time
+# to patch the mock lib
+_patch_mock_to_raise_for_invalid_assert_calls()
 
 
 class TestCase(testtools.TestCase):
