b1b51a6ac42bc4cc63ab326c04fb4b6f4ad36ac7
Tony Breeds <tony@bakeyournoodle.com>
2015-11-16 14:00:09 +1100

    subunit2html has moved.
    As per: http://lists.openstack.org/pipermail/openstack-dev/2015-August/072982.html
    
    call subunit2html from it's new path.
    
    Related-Bug: 1491646
    Change-Id: I661c55c5b6bc0ef6a6f102f4178b561dcac4883f

--- novaclient/tests/functional/hooks/post_test_hook.sh
+++ novaclient/tests/functional/hooks/post_test_hook.sh
@@ -18,7 +18,7 @@ function generate_testr_results {
     if [ -f .testrepository/0 ]; then
         sudo .tox/functional/bin/testr last --subunit > $WORKSPACE/testrepository.subunit
         sudo mv $WORKSPACE/testrepository.subunit $BASE/logs/testrepository.subunit
-        sudo .tox/functional/bin/python /usr/local/jenkins/slave_scripts/subunit2html.py $BASE/logs/testrepository.subunit $BASE/logs/testr_results.html
+        sudo /usr/os-testr-env/bin/subunit2html $BASE/logs/testrepository.subunit $BASE/logs/testr_results.html
         sudo gzip -9 $BASE/logs/testrepository.subunit
         sudo gzip -9 $BASE/logs/testr_results.html
         sudo chown jenkins:jenkins $BASE/logs/testrepository.subunit.gz $BASE/logs/testr_results.html.gz
