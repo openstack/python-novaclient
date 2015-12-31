openstack/python-novaclient

commit 81f8fa655ccecd409fe6dcda0d3763592c053e57

Remove python 2.6 support from python-novaclient
Since oslo is removing support for python 2.6, nova
needs to also remove support from clients and libraries.
This commit is to remove support from python-novaclient.

-- Python 2.6 compatibilty code was removed.
-- Python 2.6 was removed as a tox environment, install venv,
and from the classifiers in setup.cfg.
-- Release notes have been updated.

UpgradeImpact

Co-Authored-By: Andrey Kurilin <andr.kurilin@gmail.com>

Change-Id: I3f587ff38d478d075af5fd014e2b4b8416e185d4
Closes-bug: 1518390

novaclient/client.py
@@ -736,10 +736,6 @@ def discover_extensions(version):
 def _discover_via_python_path():
     for (module_loader, name, _ispkg) in pkgutil.iter_modules():
         if name.endswith('_python_novaclient_ext'):
-            if not hasattr(module_loader, 'load_module'):
-                # Python 2.6 compat: actually get an ImpImporter obj
-                module_loader = module_loader.find_module(name)
-
             module = module_loader.load_module(name)
             if hasattr(module, 'extension_name'):
                 name = module.extension_name

releasenotes/notes/remove-py26-support-f31379e86f40d975.yaml
@@ -1,0 +1,3 @@
+---
+upgrade:
+  - Python 2.6 support has been removed from python-novaclient.

setup.cfg
@@ -16,9 +16,10 @@ classifier =
     License :: OSI Approved :: Apache Software License
     Operating System :: OS Independent
     Programming Language :: Python
-    Programming Language :: Python :: 2.6
+    Programming Language :: Python :: 2
     Programming Language :: Python :: 2.7
-    Programming Language :: Python :: 3.3
+    Programming Language :: Python :: 3
+    Programming Language :: Python :: 3.4
 
 [files]
 packages =

tools/install_venv_common.py
@@ -17,7 +17,7 @@
 virtual environments.
 
 Since this script is used to bootstrap a virtualenv from the system's Python
-environment, it should be kept strictly compatible with Python 2.6.
+environment, it should be kept strictly compatible with Python 2.7.
 
 Synced in from openstack-common
 """
@@ -47,8 +47,8 @@ class InstallVenv(object):
         sys.exit(1)
 
     def check_python_version(self):
-        if sys.version_info < (2, 6):
-            self.die("Need Python Version >= 2.6")
+        if sys.version_info < (2, 7):
+            self.die("Need Python Version >= 2.7")
 
     def run_command_with_code(self, cmd, redirect_output=True,
                               check_exit_code=True):

tox.ini
@@ -1,6 +1,6 @@
 # noted to use py34 you need virtualenv >= 1.11.4
 [tox]
-envlist = py26,py27,py33,py34,pypy,pep8,docs
+envlist = py27,py33,py34,pypy,pep8,docs
 minversion = 1.6
 skipsdist = True
 

commit 48c9a91de6951fd428eb866fe9356c5ebf9601a1

Fix extension loading from python path on Python 2.7
Commit 81f8fa655ccecd409fe6dcda0d3763592c053e57 broke extension loading
from pythonpath on python 2.7 due to the removal of code that was
erroneously marked for 2.6 compatibility. Put it back.

Change-Id: Ic04f4d57953967fde9817ff5119ce0182453a86d
Closes-Bug: #1530230

novaclient/client.py
@@ -741,6 +741,8 @@ def discover_extensions(version):
 def _discover_via_python_path():
     for (module_loader, name, _ispkg) in pkgutil.iter_modules():
         if name.endswith('_python_novaclient_ext'):
+            if not hasattr(module_loader, 'load_module'):
+                module_loader = module_loader.find_module(name)
             module = module_loader.load_module(name)
             if hasattr(module, 'extension_name'):
                 name = module.extension_name