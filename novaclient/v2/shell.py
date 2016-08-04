5694a9a0f292d68ac56eb294aae770db789d4e05
Pavel Kholkin <pkholkin@mirantis.com>
2016-08-04 15:48:51 +0300

    remove start_version arg for keypairs v2.10 shell
    start_version is redundant because of decorator
    @api_versions.wraps("2.10") on the whole function
    
    TrivialFix
    
    Change-Id: Ib468214490e817a28640a02fb629c762e14828cb

--- novaclient/v2/shell.py
+++ novaclient/v2/shell.py
@@ -3095,8 +3095,7 @@ def do_keypair_delete(cs, args):
     '--user',
     metavar='<user-id>',
     default=None,
-    help=_('ID of key-pair owner (Admin only).'),
-    start_version="2.10")
+    help=_('ID of key-pair owner (Admin only).'))
 def do_keypair_delete(cs, args):
     """Delete keypair given by its name."""
     cs.keypairs.delete(args.name, args.user)
@@ -3125,8 +3124,7 @@ def do_keypair_list(cs, args):
     '--user',
     metavar='<user-id>',
     default=None,
-    help=_('List key-pairs of specified user ID (Admin only).'),
-    start_version="2.10")
+    help=_('List key-pairs of specified user ID (Admin only).'))
 def do_keypair_list(cs, args):
     """Print a list of keypairs for a user"""
     keypairs = cs.keypairs.list(args.user)
@@ -3161,8 +3159,7 @@ def do_keypair_show(cs, args):
     '--user',
     metavar='<user-id>',
     default=None,
-    help=_('ID of key-pair owner (Admin only).'),
-    start_version="2.10")
+    help=_('ID of key-pair owner (Admin only).'))
 def do_keypair_show(cs, args):
     """Show details about the given keypair."""
     keypair = cs.keypairs.get(args.keypair, args.user)
