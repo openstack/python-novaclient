f5b19b37382ff5c5f54abf480349c069ef285674
Sean Dague <sean@dague.net>
2016-08-12 15:02:36 -0400

    Use neutron for network name -> id resolution
    The 2.36 microversion deprecates the nova proxy APIs for looking up a
    network from neutron. The CLI will lookup a network by name for
    booting a server as a convenience.
    
    Over 90% of clouds now have neutron. We use a service catalog lookup
    to assume that neutron is available, and if not fall back to our
    proxy. This should mostly shield people from the 2.36 transition.
    
    To test this we add support in our fake client for the neutron network
    lookup. Comments were also left about some cleanups we should do in
    other parts of the shell testing for net lookup.
    
    Related to blueprint deprecate-api-proxies
    
    Change-Id: I4f809de4f7efb913c814f132f5b3482731c588c5

--- novaclient/v2/networks.py
+++ novaclient/v2/networks.py
@@ -41,6 +41,39 @@ class Network(base.Resource):
         return self.manager.delete(self)
 
 
+class NeutronManager(base.Manager):
+    """A manager for name -> id lookups for neutron networks.
+
+    This uses neutron directly from service catalog. Do not use it
+    for anything else besides that. You have been warned.
+    """
+
+    resource_class = Network
+
+    def find_network(self, name):
+        """Find a network by name (user provided input)."""
+
+        with self.alternate_service_type(
+                'network', allowed_types=('network',)):
+
+            matches = self._list('/v2.0/networks?name=%s' % name, 'networks')
+
+            num_matches = len(matches)
+            if num_matches == 0:
+                msg = "No %s matching %s." % (
+                    self.resource_class.__name__, name)
+                raise exceptions.NotFound(404, msg)
+            elif num_matches > 1:
+                msg = (_("Multiple %(class)s matches found for "
+                         "'%(name)s', use an ID to be more specific.") %
+                       {'class': self.resource_class.__name__.lower(),
+                        'name': name})
+                raise exceptions.NoUniqueMatch(msg)
+            else:
+                matches[0].append_request_ids(matches.request_ids)
+                return matches[0]
+
+
 class NetworkManager(base.ManagerWithFind):
     """
     Manage :class:`Network` resources.
