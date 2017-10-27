c363793a68816048ce7363837b36b584421ac0e7
Chen Hanxiao <chenhx@certusnet.com.cn>
2017-10-27 17:06:37 +0800

    inject file: add description of injecting multiple files.
    We support inject multiple files by one cmd.
    This patch adds some description and limitation
    in help messages.
    
    Change-Id: I481d2c83530c82e18661a13d53580501e721754f
    Signed-off-by: Chen Hanxiao <chenhx@certusnet.com.cn>

--- novaclient/v2/shell.py
+++ novaclient/v2/shell.py
@@ -590,7 +590,10 @@ def _boot(cs, args):
     dest='files',
     default=[],
     help=_("Store arbitrary files from <src-path> locally to <dst-path> "
-           "on the new server. Limited by the injected_files quota value."))
+           "on the new server. More files can be injected using multiple "
+           "'--file' options. Limited by the 'injected_files' quota value. "
+           "The default value is 5. You can get the current quota value by "
+           "'Personality' limit from 'nova limits' command."))
 @utils.arg(
     '--key-name',
     default=os.environ.get('NOVACLIENT_DEFAULT_KEY_NAME'),
@@ -1815,7 +1818,8 @@ def do_reboot(cs, args):
     dest='files',
     default=[],
     help=_("Store arbitrary files from <src-path> locally to <dst-path> "
-           "on the new server. You may store up to 5 files."))
+           "on the new server. More files can be injected using multiple "
+           "'--file' options. You may store up to 5 files."))
 @utils.arg(
     '--key-name',
     metavar='<key-name>',
