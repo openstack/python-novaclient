7ed265bbf580cb35876738f16b1d430efd75aa2a
Lee Yarwood <lyarwood@redhat.com>
2020-03-25 14:26:53 +0000

    Microversion 2.87 - Stable device boot from volume rescue
    This microversion is used to request a stable device rescue for boot
    from volume instances and will only succeed when the compute hosting the
    instance reports the COMPUTE_RESCUE_BFV trait.
    
    No changes are required within novaclient so this change only bumps
    API_MAX_VERSION.
    
    
    Depends-On: https://review.opendev.org/#/c/701430/
    
    Change-Id: I7885b8558db7657abbfe1f45877f52e947f5f655

--- novaclient/tests/unit/v2/test_shell.py
+++ novaclient/tests/unit/v2/test_shell.py
@@ -4538,6 +4538,7 @@ class ShellTest(utils.TestCase):
             83,  # There are no version-wrapped shell method changes for this.
             84,  # There are no version-wrapped shell method changes for this.
             86,  # doesn't require any changes in novaclient.
+            87,  # doesn't require any changes in novaclient.
         ])
         versions_supported = set(range(0,
                                  novaclient.API_MAX_VERSION.ver_minor + 1))
