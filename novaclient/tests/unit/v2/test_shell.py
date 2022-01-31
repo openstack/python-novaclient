94d3445e10711ff965e0ec141aaa0d0828dc43f0
whoami-rajat <rajatdhasmana@gmail.com>
2022-01-31 23:54:27 +0530

    MV 2.93 - Add support to rebuild boot volume
    This patch bumps the API microversion to 2.93 to allow
    rebuilding a volume backed instance.
    
    Implements: blueprint volume-backed-server-rebuild
    
    Depends-On: https://review.opendev.org/c/openstack/nova/+/830883
    
    Change-Id: Ie46df7ad76082e7631bb26243abed4dc3b1f40ac

--- novaclient/tests/unit/v2/test_shell.py
+++ novaclient/tests/unit/v2/test_shell.py
@@ -4699,6 +4699,7 @@ class ShellTest(utils.TestCase):
             86,  # doesn't require any changes in novaclient.
             87,  # doesn't require any changes in novaclient.
             89,  # There are no version-wrapped shell method changes for this.
+            93,  # There are no version-wrapped shell method changes for this.
         ])
         versions_supported = set(range(0,
                                  novaclient.API_MAX_VERSION.ver_minor + 1))
